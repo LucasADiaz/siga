ea7457dd75adf9a489d3d58aeeabe30e0cd3b805
DarioFlores <darioexequiel22@gmail.com>
2019-08-31 18:53:23 -0300

    Se agrego Modelo, Factory's y Migraciones de las siguientes entidades: Alumno, Materia, Preceptor, Turno, Tutor

---
+++ database/factories/PreceptorFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Preceptor;
+use Faker\Generator as Faker;
+
+$factory->define(Preceptor::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
