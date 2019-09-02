e3eab3602504f1ff11ba19fc365d6ce366e616f0
Lucas Agustin Diaz <LucasAgDiaz@gmail.com>
2019-09-02 19:51:11 -0300

    se elimino grupofactorsanguineo y se modifico la tabla documentos

--- database/migrations/2019_08_31_214430_create_documentos_table.php
+++ database/migrations/2019_08_31_214430_create_documentos_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Symfony\Component\Console\Helper\TableSeparator;
 
 class CreateDocumentosTable extends Migration
 {
@@ -14,9 +15,43 @@ class CreateDocumentosTable extends Migration
     public function up()
     {
         Schema::create('documentos', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrements('id')
+                    ->comment('identifica uniquivocamente un documento');
+
+            $table->unsignedBigInteger('legajo_id')
+                    ->comment('identifica a que legajo pertenecen los pdf ');
+
+            $table->binary('dni_pri')->comment('primera hoja del dni');
+            $table->binary('dni_seg')->comment('segunda hoda del dni');
+            $table->binary('ficha_medica')->comment('ficha medica');
+            $table->binary('const_cuil')->comment('constancia de cuil');
+            $table->binary('libreta')->comment('libreta de año pasado');
+            $table->binary('const_alum_reg')->comment('constancia de alumno regular');
+            $table->date('anio')->comment('año donde ingresa la documentación');
             $table->timestamps();
         });
+
+
+        //opcion para guardar URL en lugar del archivo
+
+        
+        /*Schema::create('documentos', function (Blueprint $table) {
+            $table->bigIncrements('id')
+                    ->comment('identifica uniquivocamente un documento');
+
+            $table->unsignedBigInteger('legajo_id')
+                    ->comment('identifica a que legajo pertenecen los pdf ');
+
+            $table->string('dni_pri')->comment('primera hoja del dni');
+            $table->string('dni_seg')->comment('segunda hoda del dni');
+            $table->string('ficha_medica')->comment('ficha medica');
+            $table->string('const_cuil')->comment('constancia de cuil');
+            $table->string('libreta')->comment('libreta de año pasado');
+            $table->string('const_alum_reg')->comment('constancia de alumno regular');
+            $table->date('anio')->comment('año donde ingresa la documentación');
+            $table->timestamps();
+        });*/
+
     }
 
     /**
