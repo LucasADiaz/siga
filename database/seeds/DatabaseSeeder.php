7668353ae6875990cdaa0f8193573948fc4bc767
johnny <johnnybarrionuevo5@gmail.com>
2019-09-08 15:29:31 -0300

    carga de datos con seeder en tabla domicilio

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use PhpParser\Node\Expr\Cast\Array_;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(DomiciliosSeeder::class);
+    }
+
+    
+    protected function truncateTable(array $tables)
+    {
+        DB::statement('SET FOREING_KEY_CHECKS = 0;');
+        foreach ($tables as $table) 
+        {
+            DB::table($tables)->truncate();  
+        }
+        DB::statement('SET FOREING_KEY_CHECKS = 1;');
     }
 }
---
+++ database/seeds/DomiciliosSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DomiciliosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+        DB::table('domicilios')->insert([       //Carga de datos de pruebas
+            'calle'=> 'Maipu ',
+            'numero'=>'482',
+            'cod_postal'=>'4700',
+            'referencias'=>'Entre Av Belgrano y Peru',
+            'piso'=>'2',
+            'departamento'=>'1',
+        ]);
+        
+        DB::table('domicilios')->insert([
+            'calle'=> 'Junin',
+            'numero'=>'594',
+            'cod_postal'=>'4700',
+            'referencias'=>'Entre Peru y Almagro',
+            'piso'=>'3',
+            'departamento'=>'2',
+        ]);
+    }
+}
