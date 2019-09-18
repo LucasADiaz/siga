7cbd6115347ad2d642afea407d8f42b6ef873d87
DarioFlores <darioexequiel22@gmail.com>
2019-09-18 19:08:32 -0300

    solucionando para jony

--- database/factories/TelefonoFactory.php
+++ database/factories/TelefonoFactory.php
@@ -7,9 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Telefono::class, function (Faker $faker) {
     return [
-        //'numero'=>$faker->e164PhoneNumber,
         'numero'=>$faker->randomElement(['+5493834772459','+5493834571346','+5493834578983','+5493834545986','+5493834756323']),
         'categoria'=>$faker->randomElement(['Casa','Trabajo','Movil']),
-        
     ];
 });
