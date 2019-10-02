a43db9540e0f382c0ae8a46705c3b670219b1ffd
DarioFlores <darioexequiel22@gmail.com>
2019-10-02 14:35:27 -0300

    listo para usar con Vue.js

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -3,13 +3,16 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Examplasasase Component</div>
+                    <div class="card-header">Ejemplo de un componente de Vue</div>
 
                     <div class="card-body">
-                       aaaaaaaaaaaaaaaaa
+                        Se puede cambiar
                     </div>
                 </div>
             </div>
+        </div>
+        <div>
+            <h1>Componente de Vue.js</h1>
         </div>
     </div>
 </template>
